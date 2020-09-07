f1d943a927e636150f51211d80900ea3e83bbc2b
VorTex <65015743+ascaxz@users.noreply.github.com>
2020-09-07 20:06:51 +0700

    Update getNil.lua

--- getNil.lua
+++ getNil.lua
@@ -1,2 +1,3 @@
 -- Used in script generator for getting nil instances
+local getnilinstances = getnilinstances or get_nil_instances
 function getNil(name,class) for _,v in pairs(getnilinstances())do if v.ClassName==class and v.Name==name then return v;end end end
